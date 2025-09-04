be95df9c1f4507868652618f3de254875ad4282a
amitjha889 <jhaamit014@gmail.com>
2025-09-04 13:24:11 +0530

    testing

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,7 @@ dotenv.config();
 
 
 const corsOptions = {
-  origin: [  
-    'http://localhost:5173',
-     'http://13.204.173.227',
-     'https://13.204.173.227' 
- 
-  ],
+  origin:'*',
   credentials: true,  
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'], 
   allowedHeaders: ['Content-Type', 'Authorization'] 
